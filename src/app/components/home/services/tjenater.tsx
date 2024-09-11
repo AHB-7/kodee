084a40e2241a2806f5811fb5d166318d11c533a3
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-09-11 23:57:53 +0200

    animation again

--- src/app/components/home/services/tjenater.tsx
+++ src/app/components/home/services/tjenater.tsx
@@ -29,18 +29,18 @@ export default function Tjenster() {
     const element = useRef(null);
     const { scrollYProgress } = useScroll({
         target: element,
-        offset: ["start 0.5", "end end"],
+        offset: ["start 0.95", "start start"],
     });
 
     return (
-        <section className="flex items-center justify-evenly flex-wrap-reverse relative max-w-3xl mx-auto">
+        <section className="flex items-center justify-center md:justify-between flex-wrap-reverse relative max-w-4xl mx-auto">
             <div className="w-full pb-12 order-3 px-1 text-center md:text-start">
                 <h2 className="text-6xl">Hva Kodee tilbyr</h2>
             </div>
             <div className="flex flex-col justify-start pt-8 px-1 md:pt-0 sm:justify-center md:pb-12 items-start gap-8">
                 <motion.ul className="tjenster-liste" ref={element}>
                     {services.map((service, i) => {
-                        const start = i / (services.length - 1);
+                        const start = i / services.length;
                         const end = (i + 1) / services.length;
                         return (
                             <Word
@@ -59,11 +59,16 @@ export default function Tjenster() {
                 transition={{ duration: 0.6, ease: "easeInOut" }}
             >
                 <p className=" text-lg px-4">
-                    Kodee tilbyr unike nettsider som skiller seg ut fra de som
-                    benytter ferdiglagde maler. Vi legger stor vekt på at våre
-                    kunder skal være synlige på nettet og ha full tilgang og
-                    kontroll over sin egen nettside. Med våre tjenester får du
-                    en pris basert på dine behov, ikke hva vi tilbyr.
+                    Kodee tilbyr{" "}
+                    <span className=" text-brand-primary">unike</span> nettsider
+                    som skiller seg ut fra de som benytter ferdiglagde maler. Vi
+                    legger stor vekt på at våre kunder skal være
+                    <span>synlige</span> på nettet og ha full tilgang og
+                    <span className="text-brand-primary">kontroll</span> over
+                    sin egen nettside. Med våre tjenester får du en{" "}
+                    <span className="text-brand-primary">pris</span> basert på
+                    dine <span className="text-brand-primary">behov</span>, ikke
+                    hva vi tilbyr.
                 </p>
             </motion.div>
         </section>
@@ -71,9 +76,9 @@ export default function Tjenster() {
 }
 
 const Word = ({ children, range, progress }: ServiceItemProps) => {
-    const opacity = useTransform(progress, range, [0.4, 1]);
+    const opacity = useTransform(progress, range, [0.3, 1]);
     return (
-        <motion.li style={{ opacity }} className="text-brand-primary  ">
+        <motion.li style={{ opacity }} className="text-brand-primary">
             <h3>{children}</h3>
         </motion.li>
     );
