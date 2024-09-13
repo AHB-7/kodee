09b37f91f735d1772df1ae35dd33990cf862859e
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-09-14 00:10:19 +0200

    last section

--- src/app/components/home/message/message.tsx
+++ src/app/components/home/message/message.tsx
@@ -1,103 +1,42 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
+import { IoMdArrowDown, IoMdArrowUp } from "react-icons/io";
+import { IoArrowDownSharp } from "react-icons/io5";
 export default function Message() {
     return (
-        <section className="flex flex-col lg:flex-row items-center justify-center lg:justify-between h-screen px-6 lg:px-16">
+        <section className="flex flex-col items-center justify-center gap-8 h-screen px-6 lg:px-16">
             <div className="mb-6 lg:mb-0 text-center lg:text-left">
-                <h2 className="text-2xl lg:text-4xl font-semibold text-brand-light">
-                    Send en melding eller bestill <br /> en time i dag
+                <h2 className="text-2xl lg:text-4xl font-semibold text-brand-light pb-4">
+                    Hva nå?
                 </h2>
+                <p className=" max-w-[24.5rem] w-full leading-7 text-justify">
+                    Kom i gang og send oss en melding eller bestill en gratis
+                    konsultasjon. Vi vil hjelpe deg med å forstå og løse dine
+                    problemer.
+                </p>
             </div>
-            <MessageForm />
+            <div className="flex gap-4">
+                <Link
+                    href="/message"
+                    className="flex items-center justify-center gap-2 border px-6 py-2  hover:text-brand-primary hover:scale-105 duration-300 active:border-brand-primary"
+                >
+                    Send En Melding
+                    <span className=" rotate-45 text-brand-primary text-xl ">
+                        <IoMdArrowUp />
+                    </span>
+                </Link>
+                <Link
+                    href="/timebestilling"
+                    className="flex items-center justify-center gap-2 border px-6 py-2  hover:text-brand-primary hover:scale-105 duration-300 active:border-brand-primary"
+                >
+                    Bestill En Time{" "}
+                    <span className=" rotate-45 text-brand-primary text-xl">
+                        <IoMdArrowUp />
+                    </span>
+                </Link>
+            </div>
         </section>
-    );
-}
-function MessageForm() {
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        message: "",
-    });
-
-    const handleChange = (
-        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        // Here you can handle form submission logic
-        console.log(formData);
-    };
-
-    return (
-        <form
-            onSubmit={handleSubmit}
-            className="w-full max-w-md p-6 rounded-md shadow-md"
-        >
-            <div className="mb-4">
-                <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="name"
-                >
-                    Name
-                </label>
-                <input
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div className="mb-4">
-                <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="email"
-                >
-                    Email
-                </label>
-                <input
-                    className="w-full bg-brand-dark px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div className="mb-6">
-                <label
-                    className="block bg-brand-dark text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="message"
-                >
-                    Message
-                </label>
-                <textarea
-                    className="w-full bg-brand-dark px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                    id="message"
-                    name="message"
-                    rows={4}
-                    value={formData.message}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <button
-                type="submit"
-                className="bg-green-500 text-brand-dark w-full px-4 py-2 rounded-md hover:bg-green-600 transition-colors"
-            >
-                Send Message
-            </button>
-        </form>
     );
 }
