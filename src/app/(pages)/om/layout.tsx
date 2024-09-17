29d472ac4a642ec56f13da818d11de097a64b409
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-09-17 21:53:48 +0200

    add nav

--- src/app/(pages)/om/layout.tsx
+++ src/app/(pages)/om/layout.tsx
@@ -1,4 +1,6 @@
+import Nav from "@/app/components/nav/nav";
 import { Metadata } from "next";
+import { Antonio } from "next/font/google";
 
 export const metadata: Metadata = {
     icons: {
@@ -43,15 +45,20 @@ export const metadata: Metadata = {
         "gründerskap og freelancing",
     ],
 };
-
+const antonio = Antonio({
+    weight: ["400", "600", "700"],
+    subsets: ["latin"],
+});
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="no">
+        <html lang="no" className={antonio.className}>
             <body className="bg-brand-dark text-brand-light mx-auto">
+                {" "}
+                <Nav />
                 {children}
             </body>
         </html>
--- src/app/(pages)/projects/layout.tsx
+++ src/app/(pages)/projects/layout.tsx
@@ -1,4 +1,6 @@
+import Nav from "@/app/components/nav/nav";
 import { Metadata } from "next";
+import { Antonio } from "next/font/google";
 
 export const metadata: Metadata = {
     icons: {
@@ -62,15 +64,20 @@ export const metadata: Metadata = {
         "konferanse nettside",
     ],
 };
-
+const antonio = Antonio({
+    weight: ["400", "600", "700"],
+    subsets: ["latin"],
+});
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="no">
+        <html lang="no" className={antonio.className}>
+            {" "}
             <body className="bg-brand-dark text-brand-light mx-auto">
+                <Nav />
                 {children}
             </body>
         </html>
